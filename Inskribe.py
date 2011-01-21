87d927df8b41d3ac037a34af402179f980a27840
thwarted <github@thwartedefforts.org>
2011-01-21 01:34:09 -0800

    bunch of changes to avoid the encoding errors, which seem to be completely unrelated to encodings explain the use of __format__, __unicode__ and __str__ avoid non-unicode strings whereever possible added repr simplefilter don't raise TypeError when no arguments are passed in make Template do something sane when given a list expose the object itself as _ expansion in ListTemplate (allowing it to be filtered completely through another subtemplate) explain the difference in how ListTemplate and Template interpretes lists

--- Inskribe.py
+++ Inskribe.py
@@ -1,12 +1,29 @@
+# vim: fileencoding=utf-8:ai:ts=4:expandtab
 
 import string
 import re
 import sys
 
+
+def unicodify(s):
+    if isinstance(s, unicode):
+        return s
+    if isinstance(s, str):
+        for decodeargs in (('utf-8',), ('iso-8859-2',), ('ascii', 'replace')):
+            try:
+                return s.decode(*decodeargs)
+            except UnicodeError, e:
+                pass
+    return s
+
+# serializing Template objects should always return unicode objects
+# which the caller need to encode in a chosen encoding to actually
+# output
+
 class Template(object):
-    pre = ''  # static string that will prefix the template output
-    t = ''    # the template itself
-    post = '' # static string that will be appended to the template output
+    pre = u''  # static string that will prefix the template output
+    t = u''    # the template itself
+    post = u'' # static string that will be appended to the template output
 
     class Formatter(string.Formatter):
         # matches things like time.strftime(%Y)
@@ -30,6 +47,7 @@ class Template(object):
             "int": lambda x: int(x),
             "float": lambda x: float(x),
             "str": lambda x: str(x),
+            "repr": lambda x: repr(x),
         }
 
         def convert_field(self, value, conversion):
@@ -65,6 +83,7 @@ class Template(object):
     _args = []
     _kwargs = {}
     _formatter = None
+    _result = False
     def __init__(self, *args, **kwargs):
         # when passed a single argument, it must be either a list or a
         # dict (and if it's a list, should be using ListTemplate instead)
@@ -80,8 +99,8 @@ class Template(object):
         if not kwargs:
             if len(args) == 1:
                 if type(args[0]) == dict:
-                    self._args = []
-                    self._kwargs = args[0]
+                    self._args = [args[0]] # entire value accessible as {0}
+                    self._kwargs = args[0] # elements of value accessible as {element}
                 elif type(args[0]) == list:
                     # TODO warn? should be using ListTemplate instead?
                     self._args = args[0]
@@ -92,7 +111,9 @@ class Template(object):
                     self._args = [args[0]]
                     self._kwargs = {}
             else:
-                raise TypeError("%s constructor takes either a single list or single dict without keyword arguments" % (self.__class__,))
+                # nothing passed in, template better not need any expansions
+                self._args = []
+                self._kwargs = {}
         else:
             if not args:
                 if len(kwargs) == 2 and 't' in kwargs and 'item' in kwargs:
@@ -113,6 +134,10 @@ class Template(object):
 
         if not self.t and self.__doc__:
             self.t = self.__doc__
+
+        self.t = unicodify(self.t)
+        self.pre = unicodify(self.pre)
+        self.post = unicodify(self.post)
 
         self.setup()
         ctx = self._find_context()
@@ -143,28 +168,67 @@ class Template(object):
         context['module'] = sys.modules[self.__module__]
         return context
 
+    # string.Formatter appears to be a maze of twisty passages
+    #
+    # .vformat needs to convert everything to a basestring-compatible format
+    # in order to actually concatentate all the parts into a single output
+    # it does this by calling
+    #  - string.Formatter.format_field(.., value, fs)
+    #  - which calls format(value, fs)
+    #  - which calls value.__format__(fs)
+    # since we use string.Formatter in render the template, subtemplates will
+    # have their own __format__ method called to be character-ized, and this
+    # then invokes stringifcation which attempts convering to the default
+    # charset, ascii, which causes encoding issues because we really should be
+    # utf8 everywhere in here.
+    # 
+    # consider: might be better done by overriding string.Formatter.format_field
+    #           above in Template.Formatter to not just blindly call format()
+    #           which ultimately calls this __format__ method.  We'd still
+    #           need to render the template though.
+    #
+    # all three, __format__, __unicode__ and __str__ are used in various places
+    # of string.Formatter to serialize objects to character streams, so we need
+    # define all three, and they all pretty much do the same thing.
+    def __format__(self, format_spec):
+        if self._result:
+            return self._result
+        o = unicodify(self._formatter.vformat(self.t, self._args, self._kwargs))
+        return format(o, format_spec)
+
+    def __unicode__(self):
+        self._result = self.__format__('')
+        return self._result
 
     def __str__(self):
-        o = self._formatter.vformat(self.t, self._args, self._kwargs)
-        return str(self.pre) + o + str(self.post)
+        return self.__unicode__()
 
 
+# ListTemplate differs from Template in how a list is interpreted when passed
+# in as the first arg.  ListTemplate will iterate over the elements, whereas
+# Template will expose them as positional expansions {0}, {1}, etc
 class ListTemplate(Template):
     _sequence_labeler = None # need some interface to set this
     def __init__(self, *args, **kwargs):
         self._sequence_labeler = Labelers.simple_numbers()
         super(ListTemplate, self).__init__(*args, **kwargs)
 
-    def __str__(self):
-        o = ''
+    def __format__(self, format_spec):
+        if self._result:
+            return self._result
+        o = u''
         # FIXME support iterators/generators in general here, rather than just list instances
         if isinstance(self._args, list) and len(self._args) > 0:
             c = 0
             for item in self._args:
+                item['_'] = item
                 item['#'] = self._sequence_labeler.next() if self._sequence_labeler else '#'
                 # we don't merge in self._kwargs here, the item overrides it
-                o += self._formatter.vformat(self.t, self._args, item)
-        return str(self.pre) + o + str(self.post)
+                x = unicodify(self._formatter.vformat(self.t, self._args, item))
+                o += format(x, format_spec)
+                del item['_'] # break circular reference
+        return self.pre + o + self.post
+
 
 
 class Labelers(object):
